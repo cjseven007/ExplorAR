1c90bc63efc08920895de5a053afa375e9088674
CJ Seven <hperm9166@gmail.com>
2024-02-22 20:21:18 +0800

    AR clicked after plane generation, light mode crash solved

--- app/src/main/java/com/example/explorar/ar/ARActivity.java
+++ app/src/main/java/com/example/explorar/ar/ARActivity.java
@@ -5,11 +5,16 @@ import android.graphics.Point;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Display;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.explorar.MainActivity;
 import com.example.explorar.R;
 import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
 import com.google.ar.core.Pose;
 import com.google.ar.core.exceptions.CameraNotAvailableException;
 import com.google.ar.sceneform.AnchorNode;
@@ -21,6 +26,7 @@ import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
 
 import java.lang.ref.WeakReference;
@@ -28,6 +34,8 @@ import java.lang.ref.WeakReference;
 public class ARActivity extends AppCompatActivity {
     private ArFragment arFragment;
     private ArSceneView arSceneView;
+
+    private ModelRenderable modelRenderable;
     private Anchor centerAnchor;
     private String link = "https://firebasestorage.googleapis.com/v0/b/testingforvariousthings.appspot.com/o/thomas_the_tank_engine.glb?alt=media&token=8ab4b489-20f1-447e-83f3-5cad04a33c28";
 
@@ -67,9 +75,11 @@ public class ARActivity extends AppCompatActivity {
                 //modelNode.setParent(centerAnchor);
             }
         });*/
+        setUpModel();
+        setUpPlane();
 
         // Load the glb model asynchronously to optimize startup performance
-        ModelRenderable.builder()
+        /*ModelRenderable.builder()
                 .setSource(arSceneView.getContext(), R.raw.andy)
                 .build()
                 .thenAccept(modelRenderable -> {
@@ -95,6 +105,9 @@ public class ARActivity extends AppCompatActivity {
                     AnchorNode anchorNode = new AnchorNode(centerAnchor);
                     anchorNode.setParent(arFragment.getArSceneView().getScene());
                     TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
+//                    transformableNode.setLocalScale(new Vector3(0.05f,0.05f,0.05f));
+                    transformableNode.getScaleController().setMaxScale(1f);
+                    transformableNode.getScaleController().setMinScale(0.5f);
                     transformableNode.setParent(anchorNode);
                     transformableNode.setRenderable(modelRenderable);
                     arSceneView.getScene().addChild(anchorNode);
@@ -112,7 +125,7 @@ public class ARActivity extends AppCompatActivity {
                 .exceptionally(throwable -> {
                     // Handle loading errors (optional)
                     return null;
-                });
+                });*/
 
         /*BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -123,6 +136,46 @@ public class ARActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);*/
+    }
+
+    private void setUpModel() {
+        ModelRenderable.builder()
+                .setSource(this, R.raw.andy)
+                .build()
+                .thenAccept(renderable -> modelRenderable = renderable)
+                .exceptionally(throwable -> {
+                    return null;
+                });
+    }
+
+    private void setUpPlane(){
+        arFragment.setOnTapArPlaneListener(new BaseArFragment.OnTapArPlaneListener() {
+            @Override
+            public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+                Anchor anchor = hitResult.createAnchor();
+                AnchorNode anchorNode = new AnchorNode(anchor);
+                anchorNode.setParent(arFragment.getArSceneView().getScene());
+                createModel(anchorNode);
+            }
+        });
+    }
+
+    private void createModel(AnchorNode anchorNode){
+        TransformableNode transformableNode = new TransformableNode(arFragment.getTransformationSystem());
+        transformableNode.getScaleController().setMaxScale(1f);
+        transformableNode.getScaleController().setMinScale(0.5f);
+        transformableNode.setParent(anchorNode);
+        transformableNode.setRenderable(modelRenderable);
+        arSceneView.getScene().addChild(anchorNode);
+        transformableNode.select();
+
+        transformableNode.setEnabled(true);
+
+//        TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+//        node.setParent(anchorNode);
+//        node.setRenderable(modelRenderable);
+//        node.select();
+
     }
 
     @Override
