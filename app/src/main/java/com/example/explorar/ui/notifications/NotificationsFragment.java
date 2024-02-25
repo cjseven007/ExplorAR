f3fa393b46c1028abe03d46e52eb2da8c4ea12db
luhouyang <luhouyang@gmail.com>
2024-02-25 22:09:14 +0800

    connected to firebase firestore

--- app/src/main/java/com/example/explorar/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/explorar/ui/notifications/NotificationsFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.explorar.MainActivity;
+import com.example.explorar.R;
 import com.example.explorar.databinding.FragmentNotificationsBinding;
 import com.example.explorar.ui.login.LoginActivity;
 import com.example.explorar.ui.register.RegisterActivity;
@@ -21,18 +23,58 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class NotificationsFragment extends Fragment {
 
 private FragmentNotificationsBinding binding;
+    private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+    private Button button;
+    private TextView textView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
         NotificationsViewModel notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
 
-    binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        //example code for firebase
+        button = root.findViewById(R.id.get_data_button);
+        textView = root.findViewById(R.id.placeholder_text_view);
+        ArrayList<String> textList = new ArrayList<String>();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firestore.collection("courses").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                textList.add(document.toString());
+                            }
+                        } else {
+                            Toast.makeText(getContext(),
+                                            "Get data failed",
+                                            Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                        String text = "";
+                        for (String str : textList) {
+                            text = text + str;
+                        }
+                        textView.setText(text);
+                    }
+                });
+            }
+        });
+        // firebase code ends here
 
         final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
@@ -63,7 +105,7 @@ private FragmentNotificationsBinding binding;
         return root;
     }
 
-@Override
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
