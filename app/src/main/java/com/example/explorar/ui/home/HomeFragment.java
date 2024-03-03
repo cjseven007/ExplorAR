e910c376a07ecf87bca4d8b4129ad4e0118baa9d
luhouyang <luhouyang@gmail.com>
2024-03-03 18:03:15 +0800

    connected to firebase firestore

--- app/src/main/java/com/example/explorar/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/explorar/ui/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.explorar.ui.home;
 
+import static com.google.firebase.firestore.FieldPath.documentId;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,15 +12,32 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.explorar.R;
 import com.example.explorar.databinding.FragmentHomeBinding;
+import com.example.explorar.ui.courses.Courses;
+import com.example.explorar.ui.courses.CoursesAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
 
 public class HomeFragment extends Fragment {
 
 private FragmentHomeBinding binding;
 
-    ListView listView;
+    RecyclerView recyclerView;
+    CoursesAdapter coursesAdapter;
     String tutorials[] = {"Algorithms", "Data Structures", "Arduino", "Introduction to Oil and Gas"};
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
@@ -27,19 +46,37 @@ private FragmentHomeBinding binding;
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        listView = root.findViewById(R.id.module_list_view);
 
-        ArrayAdapter<String> arr = new ArrayAdapter<String>(getActivity(), R.layout.support_simple_spinner_dropdown_item, tutorials);
-        listView.setAdapter(arr);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        String uid = firebaseAuth.getCurrentUser().getUid();
+        FirebaseFirestore.getInstance().collection("users").document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                ArrayList<String> myCourses = (ArrayList<String>) task.getResult().get("courses");
+
+                Query query = FirebaseFirestore.getInstance().collection("courses").whereIn(documentId(), myCourses);
+
+                recyclerView = root.findViewById(R.id.recycler_view);
+                FirestoreRecyclerOptions<Courses> options = new FirestoreRecyclerOptions.Builder<Courses>().setQuery(query, Courses.class).build();
+                recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1));
+                coursesAdapter = new CoursesAdapter(options, getContext());
+                recyclerView.setAdapter(coursesAdapter);
+                coursesAdapter.startListening();
+                coursesAdapter.notifyDataSetChanged();
+            }
+        });
+
+
+
+
+
+//        listView = root.findViewById(R.id.module_list_view);
+//
+//        ArrayAdapter<String> arr = new ArrayAdapter<String>(getActivity(), R.layout.support_simple_spinner_dropdown_item, tutorials);
+//        listView.setAdapter(arr);
         return root;
     }
 
-
-
-@Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 }
 
---
+++ app/src/main/java/com/example/explorar/ui/user/User.java
@@ -0,0 +1,13 @@
+package com.example.explorar.ui.user;
+
+public class User {
+    String courses[];
+
+    public String[] getCourses() {
+        return courses;
+    }
+
+    public void setCourses(String[] courses) {
+        this.courses = courses;
+    }
+}
